6ea0657f3f52caabc864a0dc01d4ff1e0999a572
chalupaul <me@chalupaul.com>
2022-04-05 06:15:06 -0500

    Initial upload

---
+++ heimdall/api/app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from mangum import Mangum
+version = "v0.0.1"
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World!"}
+
+handler = Mangum(app)
