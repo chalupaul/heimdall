16b0d3e2b5ee371ba5c2881572d0df49fb653c20
chalupaul <me@chalupaul.com>
2022-05-05 12:34:50 -0500

    first cdk deploy

--- cdk/layers.py
+++ cdk/layers.py
@@ -14,10 +14,9 @@ class Layers(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        site_dir = os.path.join(paths.venv_root, "Lib", "site-packages")
         api_layer = lambda_.LayerVersion(self, "Heimdall-API-Layer",
             removal_policy=RemovalPolicy.DESTROY,
-            code=lambda_.Code.from_asset(site_dir),
+            code=lambda_.Code.from_asset(paths.layer_dir),
             compatible_runtimes=[lambda_.Runtime.PYTHON_3_9],
         )
         self.api_layers: Sequence[lambda_.ILayerVersion] = [api_layer]
