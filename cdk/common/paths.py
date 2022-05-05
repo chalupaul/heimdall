7bf1038c55e9cf107c288d2568bd19c6bacc2a0f
chalupaul <me@chalupaul.com>
2022-05-05 14:21:07 -0500

    fixing layer paths

--- cdk/common/paths.py
+++ cdk/common/paths.py
@@ -4,6 +4,7 @@ import os
 base_root = Path(__file__).parent.parent.parent.resolve()
 venv_root = os.path.join(base_root, '.venv')
 api_root = os.path.join(base_root, "heimdall", "api")
+
 build_root = os.path.join(base_root, "build")
-layer_dir = os.path.join(base_root, "lambda_layers")
+layer_dir = os.path.join(build_root, "lambda_layers")
 pip_target_dir = os.path.join(build_root, layer_dir, "python")
