8144f9110963e1a47befe044e36e4fdfc4018ae2
chalupaul <me@chalupaul.com>
2022-05-05 19:07:59 -0500

    wip

--- cdk/layers.py
+++ cdk/layers.py
@@ -15,7 +15,7 @@ class Layers(NestedStack):
         super().__init__(scope, construct_id, **kwargs)
 
         api_layer = aws_lambda.LayerVersion(self, "Heimdall-API-Layer",
-            removal_policy=RemovalPolicy.RETAIN,
+            removal_policy=RemovalPolicy.DESTROY,
             code=aws_lambda.Code.from_asset(paths.layer_dir),
             compatible_runtimes=[aws_lambda.Runtime.PYTHON_3_9],
         )
