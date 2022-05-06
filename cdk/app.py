9050d98a24929f9acb645e0321443e468a91a9e7
chalupaul <me@chalupaul.com>
2022-05-05 19:10:58 -0500

    wip

--- cdk/app.py
+++ cdk/app.py
@@ -16,8 +16,8 @@ class MainStack(Stack):
     def __init__(self, scope):
         super().__init__(scope, "Heimdall")
 
-        layers = Layers(scope, "Heimdall-LambdaLayers")
-        api = Api(scope, "Heimdall-API", layers=layers.layers)
+        layers = Layers(self, "Heimdall-LambdaLayers")
+        api = Api(self, "Heimdall-API", layers=layers.layers)
 
 #pipelines = Pipelines(app, "heimdall-pipelines")
 
